aefab1908cb38ba57202fbe743f48173931e2267
Alan Pich <alan.pich@gmail.com>
2012-09-16 20:34:37 +0100

    Display file name & dynamic preview

--- assets/components/tveasyupload/js/mgr/easyupload.button.js
+++ assets/components/tveasyupload/js/mgr/easyupload.button.js
@@ -12,6 +12,10 @@ EasyUpload.Button = function(config) {
 			action: 'browser/file/upload'
 		},
 		items: [{
+			xtype: 'displayfield',
+			name: 'filename',
+			value: 'ARSE'
+		},{
 			xtype: 'fileuploadfield',
 			buttonText: 'Select file...',
 			buttonOnly: true,
@@ -32,13 +36,13 @@ EasyUpload.Button = function(config) {
 				this.tvField = document.getElementById(this.tvFieldId);
 			
 				// Bind clicks to auto upload file on selection
-				this.items.items[0].fileInput.dom.eub = this;
-				this.items.items[0].fileInput.dom.addEventListener('change',function(){
+				this.items.items[1].fileInput.dom.eub = this;
+				this.items.items[1].fileInput.dom.addEventListener('change',function(){
 						this.eub.onInputFileChange();
 					});
 					
 				// Fix dodgy upload button positioning
-				var wrap = Ext.get(this.renderTo).select('div.x-form-element').elements[0];
+				var wrap = Ext.get(this.renderTo).select('div.x-form-element').elements[1];
 					wrap.style.paddingLeft = '1px';
 					
 				// Create the preview image img
@@ -47,12 +51,17 @@ EasyUpload.Button = function(config) {
 				this.previewImage.style.marginTop = '5px';
 				this.previewImage.src = this.tvField.value;
 				document.getElementById(this.renderTo).appendChild(this.previewImage);
+				// Hide img if file or not set
+				this.showHidePreview();
 				
 				// Set the tv_id and res_id fields
 				var resid = this.getField('res_id');
 					resid.setValue(this.res_id);
 				var tvid = this.getField('tv_id');
 					tvid.setValue(this.tv_id);
+					
+				// Update the filename (if it exists)
+				this.updateFileNameField();
 				
 			},scope: this},
 			'success': {fn:function(res){
@@ -65,6 +74,12 @@ EasyUpload.Button = function(config) {
 
 				// Update the preview image
 				this.updatePreview(URL);
+				
+				// Update the file name field
+				this.updateFileNameField();
+				
+				// Show/Hide preview image based on file type
+				this.showHidePreview();
 
 				
 				// Mark resource as Dirty
@@ -80,7 +95,7 @@ Ext.extend(EasyUpload.Button,MODx.FormPanel,{
 
 	
 onInputFileChange: function(){
-		var filename = this.items.items[0].fileInput.dom.value.split('\\').pop();
+		var filename = this.items.items[1].fileInput.dom.value.split('\\').pop();
 		this.filename = filename;
 		this.submit();
 	}//
@@ -88,8 +103,32 @@ onInputFileChange: function(){
 
 ,updatePreview: function(URL){
 		this.previewImage.src = URL;
+		this.getField('filename').setValue('HELLLOOOOO');
 	}//
 
+,showHidePreview: function(){
+		var src = this.tvField.value;
+		if(src == ''){
+			this.previewImage.style.display="none";
+		} else {
+			// Grab file extension
+			var images = Array('png','jpg','gif','jpeg','bmp');
+			var ext = src.split('.').pop();
+			if( images.indexOf(ext) != -1){
+				// Is an image file, show the preview
+				this.previewImage.style.display = 'block';
+			} else {
+				this.previewImage.style.display = 'none';
+			};
+		}
+	}//
+
+
+,updateFileNameField: function(){
+		var src = this.tvField.value;
+		var filename = src.split('/').pop();
+		this.getField('filename').setValue(filename);
+	}//
 
 });
 Ext.reg('easyupload-button',EasyUpload.Button);
