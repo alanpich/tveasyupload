3b98af0551a04bd9676f967300560cdf9fac9a6d
Alan Pich <alan@alan-desktop.(none)>
2013-02-21 18:44:26 +0000

    Added missing OnDocFormPrerender event to plugin installer

--- _build/data/transport.plugin.events.php
+++ _build/data/transport.plugin.events.php
@@ -7,13 +7,14 @@ $events = array();
 
 $evs = array(
     'OnTVInputRenderList',
-    'OnTVInputPropertiesList'
+    'OnTVInputPropertiesList',
+    'OnDocFormPrerender'
 );
 
 foreach ($evs as $ev) {
     $events[(string)$ev] = $modx->newObject('modPluginEvent');
     $events[(string)$ev]->fromArray(array(
-        'event' => (string)$ev,
+        'event' => (string) $ev,
         'priority' => 0,
         'propertyset' => 0,
     ),'',true,true);
